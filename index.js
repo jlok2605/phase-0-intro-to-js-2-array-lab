6248575d2464591c42366bc2aae914bcf23dbaf6
Jeremy Lok <lambourghinis_pone@hotmail.com>
2023-03-23 18:43:00 +1000

    lab finished

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Write your solution here!
+const cats = ["Milo","Otis","Garfield"]
+
+function destructivelyAppendCat(cat){
+    cats.push(cat);
+
+}
+
+function destructivelyPrependCat(cat){
+    cats.unshift(cat)
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+    
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function appendCat(cat){
+    const newCats = [...cats,cat]
+    return newCats;
+}
+
+function prependCat(cat){
+    const newCats = [cat,...cats]
+    return newCats;
+}
+
+function removeLastCat(){
+    const newCats = cats.slice(0,-1)
+    return newCats;
+
+}
+
+function removeFirstCat(){
+    const  newCats = cats.slice(1)
+    return newCats
+}
